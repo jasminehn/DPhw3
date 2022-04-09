f21e43595f3c3f408632e4c0a67dbad4e0788f0a
SentientBagel <jhnelson.contact@gmail.com>
2022-04-09 02:55:16 -0400

    kms

--- script.js
+++ script.js
@@ -17,6 +17,33 @@ function ready() {
     document.getElementsByClassName('place-order')[0].addEventListener('click', purchaseClicked)
     alert("what")
     */
+    // Hide cart and terms on load.
+    var rechargewindow = document.getElementsByClassName('rechargewindow')[0]
+    var homewindow = document.getElementsByClassName('gamewindow')[0]
+
+    rechargewindow.style.display = 'none'
+    // Show products, hide cart.
+    var showProductsBtn = document.getElementsByClassName('show-recharge')[0]
+    showProductsBtn.addEventListener('click', function() {
+        if (rechargewindow.style.display != 'block') {
+            rechargewindow.style.display = 'block'
+        }
+        if (homewindow.style.display != 'none') {
+            homewindow.style.display = 'none'
+        }
+    })
+
+    // Show cart, hide products.
+    var showCartBtn = document.getElementsByClassName('show-home')[0]
+    showCartBtn.addEventListener('click', function() {
+        if (homewindow.style.display != 'block') {
+            homewindow.style.display = 'block'
+        }
+        if (rechargewindow.style.display != 'none') {
+            rechargewindow.style.display = 'none'
+        }
+    })
+
     var quantityInputs = document.getElementsByClassName('quantity')
     for (var i = 0; i < quantityInputs.length; i++) {
         var input = quantityInputs[i]
@@ -32,6 +59,47 @@ function ready() {
     if(getbonusElement){
         getbonusElement.addEventListener('click', getBonus)
     }
+
+    /*
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+        content.style.display = "none";
+        } else {
+        content.style.display = "block";
+        }
+    });
+    }
+    */ 
+
+    var collapse = document.getElementsByClassName("collapse")[0]
+    //var terms = document.getElementByClassName("terms-content");
+    if(collapse){
+        collapse.addEventListener('click', function() {
+            //this.classList.toggle("active");
+            //var content = this.nextElementSibling;
+            var terms = document.getElementsByClassName("terms-content")[0]
+            if(terms){
+                if (terms.style.display === "block") {
+                    terms.style.display = "none";
+                } else {
+                    terms.style.display = "block";
+                }
+            }
+            
+        });
+    }
+    
+    var daysuntil = document.getElementById("daysuntil");
+    if(daysuntil){
+        daysuntil.innerHTML = 7 - clicks;
+    }
+    
 }
 
 var clicks = 1;
@@ -39,8 +107,10 @@ var clicks = 1;
 function claimBonus() {
     clicks += 1;
     document.getElementById("file").value = clicks;
+    daysuntil.innerHTML = 7 - clicks;
+    
     if (document.getElementById("file").value == 7) {
-        //alert('You\'ve earned this week\'s bonus!!!')
+        alert('You\'ve earned this week\'s bonus!!!')
         unlockBonus()
         return
     }
@@ -62,9 +132,9 @@ var silverbucks = 0;
 var brassbucks = 0;
 
 function updateBucks(){
-    document.getElementsByClassName('goldbucks').textContent = goldbucks
-    document.getElementsByClassName('silverbucks').textContent = silverbucks
-    document.getElementsByClassName('brassbucks').textContent = brassbucks
+    document.getElementsByClassName('goldbucks')[0].textContent = goldbucks + 1
+    document.getElementsByClassName('silverbucks')[0].textContent = silverbucks
+    document.getElementsByClassName('brassbucks')[0].textContent = brassbucks + 69 //nice
 }
 
 function updateCartTotal() {
@@ -103,7 +173,7 @@ function updateCartTotal() {
     if (!failed) {
         document.getElementsByClassName('cart-subtotal')[0].textContent = '$' + subtotal
         document.getElementsByClassName('cart-total-price')[0].textContent = '$' + total
-        document.getElementsByClassName('agree-price')[0].textContent = '$' + total
+        //document.getElementsByClassName('agree-price')[0].textContent = '$' + total
     }else{
         document.getElementsByClassName('cart-total-price')[0].textContent = 'ERROR'
     }
@@ -115,9 +185,10 @@ function quantityChanged(event) {
         input.value = 1
     }
     updateCartTotal()
-    goldbucks = document.getElementsById('gold-quantity');
-    silverbucks = document.getElementsById('gold-quantity');
-    brassbucks = document.getElementsById('gold-quantity');
+
+    goldbucks = Number(document.getElementById('gold-quantity').value);
+    silverbucks = Number(document.getElementById('silver-quantity').value);
+    brassbucks = Number(document.getElementById('brass-quantity').value);
 }
 
 
